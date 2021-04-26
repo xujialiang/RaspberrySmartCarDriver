45300a0997625e3cf1814dfadb45e0d66494241c
xujialiang <mail@xujialiang.net>
2021-04-26 23:47:10 +0800

    控制左右

--- SmartCarDriver.js
+++ SmartCarDriver.js
@@ -25,7 +25,12 @@ class SmartCarDriver {
     start(){
         let that = this;
         SBUS.start('/dev/ttyAMA0', (status, channels, channels_c)=>{
-            // 处理各个通道数据, 通过Promise异步处理
+            that._processSpeedAndDirection(status, channels, channels_c);
+        });
+    }
+
+    _processSpeedAndDirection(status, channels_origin, channels_convert){
+        // 处理各个通道数据, 通过Promise异步处理
             // 通道1:右手左右;    控制左右方向
             // 通道2:左手上下;  
             // 通道3:右手上下;    油门 + -, 如果通道1有指令向左右，则按通道1方向。 如果通道1没有设置左右方向，则按默认向前或向后。
@@ -36,38 +41,46 @@ class SmartCarDriver {
             // 通道8:SW4;       打开关闭啥？
             // 通道9:RV左;      云台左右
             // 通道10:RV右;     云台上下 
-            console.log('油门', new Date(),status, channels[2], channels_c[2]);
-
-            that._processSpeedAndDirection(status, channels[2], channels_c[2]);
-            
-        });
-    }
-
-    _processSpeedAndDirection(status, channel_origin, channel_convert){
-        if(channel_origin == 0 || channel_convert == 0 || channel_origin == 200) {
+        console.log('油门', new Date(),status, channels[2], channels_c[2]);
+        console.log('左右', new Date(),status, channels[0], channels_c[0]);
+        if(channels_origin[2] == 0 || channels_convert[2] == 0 || channels_origin[2] == 200) {
             return;
         }
         if(status==0){
-            const diff = channel_convert - 0.5;
+            const diff = channel_convert[2] - 0.5;
             var diff_abs = Math.abs(diff);
-            if(diff_abs>0.1){
-                // 控制速度
+            if(diff_abs>=0.1){
+                // 控制油门速度
                 if(diff_abs > 0.5){
                     diff_abs = 0.5;
                 }
                 const percent = parseInt(diff_abs*2*100);
                 console.log('percent', percent);
-
                 motorMgr.updateSpeed(percent, this.leftFront);
 
-                // 控制方向
-                if(diff>0){
-                    console.log('向前');
-                    motorMgr.doForward();
+                // 判断是否有左右, 如果是左右，优先级高于前后
+                const diff_leftright = channel_convert[0] - 0.5;
+                var diff_leftright_abs = Math.abs(diff_leftright);
+                if(diff_leftright_abs>=0.25){
+                    // 控制左右方向
+                    if(diff_leftright>0){
+                        motorMgr.doLeft();
+                    }else{
+                        motorMgr.doRight();
+                    }
                 }else{
-                    console.log('向后');
-                    motorMgr.doBackward();
+                    // 控制前后方向
+                    if(diff>0){
+                        console.log('向前');
+                        motorMgr.doForward();
+                        return;
+                    }else{
+                        console.log('向后');
+                        motorMgr.doBackward();
+                        return;
+                    }
                 }
+                
             }else{
                 motorMgr.updateSpeed(0);
             }
